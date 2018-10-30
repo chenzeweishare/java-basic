ae32aaa2838eb8d5c88949848fa6c40b5e2270a3
chenzeweishare <185401774@qq.com>
2018-10-30 23:19:23 +0800

    jvm更新版本

--- src/main/java/com/czw/basic/Jvm/OOMTest.java
+++ src/main/java/com/czw/basic/Jvm/OOMTest.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * 堆初始10M, 最大10M, 打印Heap信息, 把当时快照保存
+ */
 public class OOMTest {
-	// JVM设置    
-	// -Xms10M -Xmx10M -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=D:\jvm.dump 
+	// JVM设置 当时情况   
+	// -Xms10M -Xmx10M -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=H:\jvm.dump
 	public static void main(String[] args) {
 		List<Object> list = new ArrayList<>();
 		int i = 0;
