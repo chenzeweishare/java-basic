1530810500e42c460aeea44cfe3c6dd192a0259f
185401774@qq.com <185401774@qq.com>
2019-03-30 16:07:51 +0800

    commit

--- src/main/java/com/czw/basic/mq/active/Consumer.java
+++ src/main/java/com/czw/basic/mq/active/Consumer.java
@@ -17,7 +17,7 @@ public class Consumer {
 	@JmsListener(destination = "queue1", containerFactory = "jmsQueueListener")
 	public void receiveQueue(final TextMessage text, Session session) throws JMSException {
 		try {
-			logger.info("Consumer收到的报文为:" + text.getText());
+			//logger.info("Consumer收到的报文为:" + text.getText());
             // 使用手动签收模式，需要手动的调用，如果不在catch中调用session.recover()消息只会在重启服务后重发
 			//text.acknowledge();
 		} catch (Exception e) {
