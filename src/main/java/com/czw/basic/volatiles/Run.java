84098e6af7b6e81ae1c117ce7b9099f3f73f0c8c
185401774@qq.com <185401774@qq.com>
2019-03-30 11:05:30 +0800

    commit

---
+++ src/main/java/com/czw/basic/volatiles/Run.java
@@ -0,0 +1,13 @@
+package com.czw.basic.volatiles;
+
+public class Run {
+	public static void main(String[] args) throws InterruptedException {
+
+		RunThread thread = new RunThread();
+		thread.start();
+		Thread.sleep(1000);
+		thread.setRunning(false);
+
+		System.out.println("已经赋值为false");
+	}
+}
